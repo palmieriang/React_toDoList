9b7c0ba1b7383bb1686954ab3b9dc68e25fb845d
Angelo Palmieri <palmieri.ang@gmail.com>
2017-07-23 22:41:13 +0100

    remove task

--- src/ToDoAppList.jsx
+++ src/ToDoAppList.jsx
@@ -6,9 +6,14 @@ class ToDoAppList extends  Component {
       console.log(props)
   }
 
+  remove(elem) {
+    var value = elem.target.parentNode.querySelector('span').innerText
+    this.props.remove(value)
+  }
+
   render() {
     var task = this.props.tasks.map((elem, i) => {
-      return <li key={i}>{elem}</li>
+      return <li key={i}><span>{elem}</span><button onClick={this.remove}>X</button></li>
     })
     return (
       <ul>
